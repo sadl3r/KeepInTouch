75ed00d1b5878ed1989afb5fb06957afd2a9c19c
root <root@mehdi-HP-EliteBook-8530w.(none)>
2013-06-01 09:54:48 +0200

    First commit

---
+++ KeepInTouch/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KeepInTouch::Application.config.secret_token = '2807f7f304611102edd1a86f9b74a7305b464e2b4678cd7208c48430f60882f399ba134974c89a5c107bd044017ef3834270a1bdd08d891c1f14e136257efe8e'
